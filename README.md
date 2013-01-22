b6c40edbd9da1976c901b7d97f10adfa0049c3fc
Eldhose John <dits59@yahoo.com>
2013-01-22 19:27:47 +0530

    Proj files added
    S3 and bash scripts

---
+++ README.md
@@ -0,0 +1,18 @@
+
+Automatic MongoDB Backup
+
+Simple bash script to configure automatic database backup for mongodb
+
+Required
+
+Boto S3 [pip install boto]
+
+s3multiput or s3cmd comes for Amazon [apt-get install s3cmd]
+
+FileChunkIO [easy_install FileChunkIO]
+
+--Credits--
+
+http://sunilarora.org/database-backup-from-mongodb-to-amazon-s3-and
+http://www.xorcode.com/2012/08/22/round-robin-mongodb-backups-to-s3-with-tar/
+http://www.cyberciti.biz/faq/how-do-i-add-jobs-to-cron-under-linux-or-unix-oses/
---
+++ backup.sh
@@ -0,0 +1,50 @@
+
+#!/bin/bash
+
+MONGODB_SHELL='mongo' #otherwise location
+DUMP_UTILITY='mongodump'
+DB_NAME='c'
+
+USERNAME=""
+PASSWORD=""
+ 
+SCRIPT_DIR="/home/ubuntu/db-backup/mongodb-auto-backup"
+
+date_now=`date +%Y_%m_%d_%H_%M_%S`
+#dir_name='db_backup_'${date_now}
+#file_name='db_backup_'${date_now}'.bz2'
+
+
+BACKUP_DIR_NAME=${SCRIPT_DIR}'/db_backup_'${date_now}
+BACKUP_FILE_NAME='db_backup_'${date_now}'.bz2'
+BACKUP_FILE_LOCN=${SCRIPT_DIR}'/'${DB_NAME}
+
+FSYNC_FILE=${SCRIPT_DIR}'/fsync_lock.js'   #The MongoDB Lock file to avoid data writes
+UNLCK_FILE=${SCRIPT_DIR}'/unlock.js'  #The MongoDB unlock file
+S3_FILE=${SCRIPT_DIR}'/aws_s3.py'  #Python script to pload to S3
+ 
+log() {
+    echo $1
+}
+ 
+do_cleanup(){
+    rm -r  ${BACKUP_DIR_NAME}
+    rm ${BACKUP_FILE_NAME}    
+    log 'cleaning up....'
+}
+ 
+do_backup(){
+    log 'snapshotting the db and creating archive' && \
+    ${MONGODB_SHELL} admin ${FSYNC_FILE} && \
+    ${DUMP_UTILITY} -d ${DB_NAME} -u ${USERNAME} -p ${PASSWORD} -o ${BACKUP_DIR_NAME} && tar jcfP ${BACKUP_FILE_NAME} ${BACKUP_DIR_NAME}
+    ${MONGODB_SHELL} admin ${UNLCK_FILE} && \
+    log 'data backd up and created snapshot'
+}
+ 
+save_in_s3(){
+    log 'saving the backup archive in amazon S3' && \
+    python ${S3_FILE} set ${DB_NAME} ${BACKUP_FILE_NAME} && \
+    log 'data backup saved in amazon s3'
+}
+ 
+do_backup && save_in_s3 && do_cleanup
